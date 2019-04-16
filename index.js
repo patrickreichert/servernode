9980efcaa1b55cf2273b47af5f85f920c7deb4ab
Patrick <patrick.reichert@tecnicosuperiorekennedy.it>
2019-04-16 10:24:29 +0200

    primo server node.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//Semplice web server
+//Modulo HTTP
+const http = require('http');
+
+//Comportamento er server con callback
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('Ciao mondo!');
+    res.end();
+})
+
+//In ascolto
+//server.listen(porta, url);
+server.listen(8080, '127.0.0.1');
+
+console.log('Server running at http://127.0.0.1:8080/');
