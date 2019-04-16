4881484819aae4f8e47588c13288756e5d407765
Patrick <patrick.reichert@tecnicosuperiorekennedy.it>
2019-04-16 10:41:35 +0200

    cambio porta webserver

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ const server = http.createServer(function (req, res) {
 
 //In ascolto
 //server.listen(porta, url);
-server.listen(8080, '127.0.0.1');
+server.listen(8081, '127.0.0.1');
 
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1:8081/');
