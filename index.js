ab6c7eed0412a49368e435aafe2508fc62b56187
Patrick <patrick.reichert@tecnicosuperiorekennedy.it>
2019-04-19 11:45:45 +0200

    update

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const express = require('express')
-let port = process.argv[3] || 3000
+//let port = process.env.PORT || 3000
+let port = process.argv[2] || 3000
 const app = express()
 
-//Mostra la porta
-console.log(" Port =", process.env.port) 
-//Mostra il percorso dove è installato 'node' e la cartella che contiene il file 'express.js'
-console.log('\n', process.argv)
-//Mostrami la versione dell'app nal caso scrivessi ' node express.js v ' nel terminale
-if(process.argv[2] === 'v')
-{
-  console.log('\n', 'MyApp versione 0.0.1')
-}
+/*
+  //Mostra la porta
+  console.log(" Port =", process.env.port) 
+  //Mostra il percorso dove è installato 'node' e la cartella che contiene il file 'express.js'
+  console.log('\n', process.argv)
+  //Mostrami la versione dell'app nal caso scrivessi ' node express.js v ' nel terminale
+  if(process.argv[2] === 'v')
+  {
+    console.log('\n', 'MyApp versione 0.0.1')
+  } 
+*/
 
+//Diciamo che la ROOT dei file statici è la cartella 'public'
+//app.use(express.static(__dirname + '/nome_cartella_file_statici'))
+app.use(express.static(__dirname + '/public'))
+
+/*
 app.get
 (
   '/',
@@ -20,6 +28,7 @@ app.get
     response.send('Hello World')
   }
 )
- 
+*/
+
 app.listen(port)
 console.log('\n', `Server running at http://127.0.0.1:${port}`);
