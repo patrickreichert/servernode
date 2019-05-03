9b0aa253b83311167026178acaff1744998c65e1
Patrick <patrick.reichert@tecnicosuperiorekennedy.it>
2019-05-03 11:29:02 +0200

    routersss

--- index.js
+++ index.js
@@ -1,9 +1,47 @@
 const express = require('express')
 const app = express()
-const fs = require('fs')
+const {settings} = require('./settings')
+const routes = require('./routes')
+const users = require('./routes/users')
+const personaggi = require('./routes/personaggi')
 let port = process.argv[2] || 8080
+console.log(settings)
 
-app.get
+app.use
+(
+  '/',
+  routes
+)
+
+app.use
+(
+  '/users',
+  users
+)
+
+app.use
+(
+  '/personaggi',
+  personaggi
+)
+
+app.listen(port)
+
+/*app.get
+( 
+  '/',
+  function(req, res)
+  {
+    res.send('Ciao')
+  }
+)*/
+
+
+
+
+
+
+/* app.get
 (
   '/books',
   function(req, res)
@@ -63,6 +101,4 @@ app.get
                   }
                )
   }
-)
-
-app.listen(port)
+) */
---
+++ settings.js
@@ -0,0 +1,12 @@
+const test = {
+                prototipo: 0.01
+             }
+
+//Creo un oggetto settings
+const settings = {
+                    versione: 0.01,
+                    mode: 'dark',
+                    location: 'Trieste'
+                 }
+
+exports.settings = settings
